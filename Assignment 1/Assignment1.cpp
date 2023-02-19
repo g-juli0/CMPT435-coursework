6f85739f6d2553d5ad7071d7661d73debb5ff1bd
Gianna Julio <46408848+g-juli0@users.noreply.github.com>
2023-02-19 17:19:39 -0500

    doc outline, node class, beginning linked list

--- Assignment 1/Assignment1.cpp
+++ Assignment 1/Assignment1.cpp
@@ -1,16 +1,95 @@
+// Gianna Julio
 
 #include <iostream>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!" << std::endl;
+    cout << "Hello World!" << endl;
     return 0;
 }
 
 // node class
+class Node
+{
+public:
+
+    int value;
+    Node* pointer;
+
+    // default constructor
+    Node()
+    {
+        value = 0;
+        pointer = NULL;
+    }
+
+    // overloaded constructor
+    Node(int v)
+    {
+        value = v;
+        pointer = NULL;
+    }
+};
 
 // singly linked list
+class SinglyLinkedList
+{
+public:
+    Node* head;
+    Node* tail;
+    int length;
 
-// stack
+    SinglyLinkedList()
+    {
+        head = NULL;
+        tail = NULL;
+        length = 0;
+    }
 
-// queue
+    // insert
+    void insert(int pos)
+    {
+
+    }
+    // remove
+    void remove(int pos)
+    {
+
+    }
+    // print
+    void print()
+    {
+        Node* temp = head;
+
+        // if list is empty
+        if (head == NULL)
+        {
+            cout << "Empty list" << endl;
+            return;
+        }
+
+        while (temp != NULL)
+        {
+            cout << temp->pointer << endl;
+            temp = temp->pointer;
+        }
+
+    }
+    // get
+    // set
+};
+
+// stack - first in, last out
+class Stack
+{
+    // push
+    // pop
+};
+
+// queue - first in, first out
+class Queue
+{
+    // enqueue
+    // dequeue
+};
