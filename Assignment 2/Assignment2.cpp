3f0d2cfcfc84bc458d3eb78d3eb960bbe949951b
Gianna Julio <46408848+g-juli0@users.noreply.github.com>
2023-02-23 01:22:14 -0500

    program outline

---
+++ Assignment 2/Assignment2.cpp
@@ -0,0 +1,39 @@
+// Gianna Julio
+
+#include <iostream>
+using namespace std;
+
+// initialize array as global variable
+string items[666];
+
+// selection sort
+// insertion sort
+// merge sort
+// quick sort
+
+int main()
+{
+    cout << "Hello World!" << endl;
+
+    /* SAMPLE OUTPUT
+    n^2 : 443,556.000
+    n*log(n): 6,246.667
+
+    Bubble sort --------------------
+    Number of comparisons: 221,445
+    This took : 7,402 nano?s
+
+    Insertion sort -----------------
+    Number of comparisons: 109,610
+    This took : 2,848 nano?s
+
+    Merge sort ---------------------
+    Number of comparisons: 5,419
+    This took : 586 nano?s
+
+    Quicksort ----------------------
+    Number of comparisons: 7,441
+    This took : 465 nano?s
+    */
+
+}
