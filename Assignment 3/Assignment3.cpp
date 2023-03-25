da1020c1f9ee1c23e79a33d7dd6ec124650bbe52
Gianna Julio <46408848+g-juli0@users.noreply.github.com>
2023-03-24 21:59:50 -0400

    file created

---
+++ Assignment 3/Assignment3.cpp
@@ -0,0 +1,15 @@
+// Gianna Julio
+
+#include <iostream>             // file reading
+#include <fstream>              // file reading
+#include <string>               // strings
+#include <algorithm>            // transform entire strings to lowercase for sorting
+using namespace std;
+
+// initialize array as global variable
+string items[666];
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
